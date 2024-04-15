28a79034d2f46f686b4674e8bca6ef3779dc5363
fabriciond <fabricio.nunez229115@potros.itson.edu.mx>
2024-04-15 16:38:21 -0700

    module integration

--- Products/app.js
+++ Products/app.js
@@ -1,6 +1,14 @@
-const express =  require('express');
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const productRoutes = require('./Routes/Product');
+app.use('/products', productRoutes())
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
