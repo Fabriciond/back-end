9cd13e1a2d88c0c9013f215dd5922a8d5577170e
fabriciond <fabricio.nunez229115@potros.itson.edu.mx>
2024-04-16 00:59:20 -0700

    add jwt

---
+++ Products/Config/jwt.js
@@ -0,0 +1,17 @@
+const jose = require('jose'); 
+const secret = new TextEncoder().encode('Marianaaaaaaa');
+const alg = "HS256";
+
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ message: 'Token is required' });
+    try {
+        const decoded = jose.jwtVerify(token, secret);
+        req.user = decoded.username;
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: 'Invalid token' });
+    }
+};
+module.exports = { verifyToken };
--- Products/app.js
+++ Products/app.js
@@ -2,16 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
+const jose = require('jose');
+const { verifyToken } = require('./Config/jwt');
 app.use(cors());
 app.use(bodyParser.json());
 
+
 const productRoutes = require('./Routes/Product');
-app.use('/products', productRoutes())
+app.use('/products', verifyToken ,productRoutes())
 
-
-app.get('/', (req, res) => {
+app.get('/', verifyToken, (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (username !== 'admin' || password !== 'admin') {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const jwt = await new jose.SignJWT({ username: 'admin' })
+        .setProtectedHeader({ alg })
+        .setExpirationTime('2h')
+        .sign(secret);
+    res.json({ token: jwt });
 });
 
 app.listen(3000, () => {
